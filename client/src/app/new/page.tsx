f162a89073430bb52b85ffc54f9730908cef0da2
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-10-15 02:10:53 +0900

    newページの追加

---
+++ client/src/app/new/page.tsx
@@ -0,0 +1,11 @@
+import Title from "../components/common/title";
+
+const New: () => JSX.Element = () => {
+  return (
+    <div className="main">
+      <Title title={"投稿を作成"} />
+    </div>
+  );
+}
+
+export default New;
