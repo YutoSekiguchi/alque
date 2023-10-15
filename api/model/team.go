6b4c17aeaf5aca3ed19940f85266fbfc4ca1d731
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-10-15 13:29:54 +0900

    passwordの追加

--- api/model/team.go
+++ api/model/team.go
@@ -8,6 +8,7 @@ import (
 type Team struct {
 	ID          int       `gorm:"primary_key;not null;autoIncrement:true"`
 	Name        string    `gorm:"type:text;not null"`
+	Password    string    `gorm:"type:text;not null"`
 	Detail      string    `gorm:"type:text;not null"`
 	Image       string    `gorm:"type:text;not null"`
 	CreatedAt   time.Time `sql:"DEFAULT:current_timestamp;column:created_at"`
