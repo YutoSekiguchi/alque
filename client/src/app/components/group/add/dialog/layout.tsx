35aeacd2082786e8f0e389dd34323237fb12f4c0
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-10-16 00:41:07 +0900

    グループ追加関係の要素の完成

--- client/src/app/components/group/add/dialog/layout.tsx
+++ client/src/app/components/group/add/dialog/layout.tsx
@@ -1,13 +1,22 @@
 import TextArea from "@/app/components/common/form/text_area/layout";
 import TextInput from "@/app/components/common/form/text_input/layout";
 import { Dispatch, SetStateAction, useState } from "react";
+import { PostTeamDataType, TeamDataType } from "../../../../../../@types/team";
+import { postTeam } from "@/services/team";
+import { PostMemberDataType } from "../../../../../../@types/member";
+import { useAtom } from "jotai";
+import { userAtom } from "@/jotai/user";
+import { postMember } from "@/services/member";
 
 interface Props {
   setIsShowDialog: Dispatch<SetStateAction<boolean>>;
+  setIsShowCompleteDialog: Dispatch<SetStateAction<boolean>>;
 }
 
 const GroupAddDialog: (props: Props) => JSX.Element = (props: Props) => {
-  const { setIsShowDialog } = props;
+  const { setIsShowDialog, setIsShowCompleteDialog } = props;
+
+  const [user, setUser] = useAtom(userAtom);
 
   const [groupName, setGroupName] = useState<string>("");
   const [groupPassword, setGroupPassword] = useState<string>("");
@@ -30,6 +39,54 @@ const GroupAddDialog: (props: Props) => JSX.Element = (props: Props) => {
     setGroupImageURL(newValue);
   }
 
+  const resetState = () => {
+    setIsShowDialog(false);
+    setGroupName("");
+    setGroupPassword("");
+    setGroupDetail("");
+    setGroupImageURL("");
+  }
+
+  const handleSubmit = async() => {
+    if (user === null) {
+      alert("ログインして下さい");
+      return;
+    }
+    if (groupName === "") {
+      alert("グループ名を入力して下さい");
+      return;
+    } else if (groupPassword.length <= 7) {
+      alert("グループのパスワードは8文字以上で入力して下さい");
+      return;
+    }
+    // dataに入力値を格納
+    const teamData: PostTeamDataType = {
+      Name: groupName,
+      Password: groupPassword,
+      Detail: groupDetail,
+      Image: groupImageURL
+    };
+    // APIを叩く
+    try {
+      const teamRes: TeamDataType = await postTeam(teamData);
+      if (teamRes === null) {
+        alert("グループ作成に失敗しました");
+        return;
+      }
+      // memberDataの作成
+      const memberData: PostMemberDataType = {
+        TID: teamRes.ID,
+        UID: user.ID,
+      }
+      await postMember(memberData);
+    } catch (e) {
+      alert("グループ作成に失敗しました");
+      return;
+    }
+    setIsShowCompleteDialog(true);
+    resetState();
+  }
+
   return(
     <div
       className="fixed top-0 left-0 w-full h-full dialog-background"
@@ -50,7 +107,9 @@ const GroupAddDialog: (props: Props) => JSX.Element = (props: Props) => {
             </div>
             <div className="flex justify-around">
               <button onClick={handleCloseDialog} className="px-4 py-2 rounded-full border hover:bg-gray-100 dark:hover:bg-gray-700">閉じる</button>
-              <button onClick={handleCloseDialog} className="bg-blue-400 text-white px-6 py-2 rounded-full border hover:bg-blue-300">作成</button>
+              <button onClick={handleSubmit} className="bg-blue-400 text-white px-6 py-2 rounded-full hover:bg-blue-300">
+                作成
+              </button>
             </div>
         </div>
     </div>
--- client/src/app/components/group/add/layout.tsx
+++ client/src/app/components/group/add/layout.tsx
@@ -3,9 +3,11 @@
 import { useState } from "react";
 import ShowAddDialogButton from "./show_add_dialog_button/layout";
 import GroupAddDialog from "./dialog/layout";
+import AutoDialog from "../../common/auto_dialog/layout";
 
 const AddGroup: () => JSX.Element = () => {
   const [isShowDialog, setIsShowDialog] = useState<boolean>(false);
+  const [isShowCompleteDialog, setIsShowCompleteDialog] = useState<boolean>(false);
 
   const handleShowDialog = () => {
     setIsShowDialog(true);
@@ -18,7 +20,11 @@ const AddGroup: () => JSX.Element = () => {
       </div>
       {
         isShowDialog&&
-        <GroupAddDialog setIsShowDialog={setIsShowDialog} />
+        <GroupAddDialog setIsShowDialog={setIsShowDialog} setIsShowCompleteDialog={setIsShowCompleteDialog} />
+      }
+      {
+        isShowCompleteDialog&&
+        <AutoDialog text={"グループを作成しました"} setIsShow={setIsShowCompleteDialog} />
       }
     </div>
   );
