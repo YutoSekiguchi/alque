2c45767258a99c0cd619dde070a0694988d30cea
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-10-17 13:40:57 +0900

    デザイン修正

--- client/src/app/components/group/add/show_add_dialog_button/layout.tsx
+++ client/src/app/components/group/add/show_add_dialog_button/layout.tsx
@@ -1,5 +1,5 @@
 const ShowAddDialogButton = () => {
-  return <button className="px-8 py-3 bg-blue-400 rounded-full text-white">新規作成</button>
+  return <button className="px-4 py-2 bg-blue-400 rounded-lg text-white hover:bg-blue-500">新規作成＋</button>
 }
 
 export default ShowAddDialogButton;
