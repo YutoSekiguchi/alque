755c538689b70d6b437f8d01662d4bc6f65ba4e8
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-10-17 13:40:19 +0900

    fix

--- client/src/app/components/group/add/dialog/layout.tsx
+++ client/src/app/components/group/add/dialog/layout.tsx
@@ -1,9 +1,9 @@
 import TextArea from "@/app/components/common/form/text_area/layout";
 import TextInput from "@/app/components/common/form/text_input/layout";
 import { Dispatch, SetStateAction, useState } from "react";
-import { PostTeamDataType, TeamDataType } from "../../../../../../@types/team";
+import { PostTeamDataType, TeamDataType } from "@/@types/team";
 import { postTeam } from "@/services/team";
-import { PostMemberDataType } from "../../../../../../@types/member";
+import { PostMemberDataType } from "@/@types/member";
 import { useAtom } from "jotai";
 import { userAtom } from "@/jotai/user";
 import { postMember } from "@/services/member";
@@ -16,7 +16,7 @@ interface Props {
 const GroupAddDialog: (props: Props) => JSX.Element = (props: Props) => {
   const { setIsShowDialog, setIsShowCompleteDialog } = props;
 
-  const [user, setUser] = useAtom(userAtom);
+  const [user, ] = useAtom(userAtom);
 
   const [groupName, setGroupName] = useState<string>("");
   const [groupPassword, setGroupPassword] = useState<string>("");
@@ -25,19 +25,19 @@ const GroupAddDialog: (props: Props) => JSX.Element = (props: Props) => {
 
   const handleCloseDialog = () => {
     setIsShowDialog(false);
-  }
+  };
   const handleChangeGroupName = (newValue: string) => {
     setGroupName(newValue);
-  }
+  };
   const handleChangeGroupPassword = (newValue: string) => {
     setGroupPassword(newValue);
-  }
+  };
   const handleChangeGroupDetail = (newValue: string) => {
     setGroupDetail(newValue);
-  }
+  };
   const handleChangeGroupImageURL = (newValue: string) => {
     setGroupImageURL(newValue);
-  }
+  };
 
   const resetState = () => {
     setIsShowDialog(false);
@@ -45,9 +45,9 @@ const GroupAddDialog: (props: Props) => JSX.Element = (props: Props) => {
     setGroupPassword("");
     setGroupDetail("");
     setGroupImageURL("");
-  }
+  };
 
-  const handleSubmit = async() => {
+  const handleSubmit = async () => {
     if (user === null) {
       alert("ログインして下さい");
       return;
@@ -64,7 +64,7 @@ const GroupAddDialog: (props: Props) => JSX.Element = (props: Props) => {
       Name: groupName,
       Password: groupPassword,
       Detail: groupDetail,
-      Image: groupImageURL
+      Image: groupImageURL,
     };
     // APIを叩く
     try {
@@ -77,7 +77,7 @@ const GroupAddDialog: (props: Props) => JSX.Element = (props: Props) => {
       const memberData: PostMemberDataType = {
         TID: teamRes.ID,
         UID: user.ID,
-      }
+      };
       await postMember(memberData);
     } catch (e) {
       alert("グループ作成に失敗しました");
@@ -85,35 +85,60 @@ const GroupAddDialog: (props: Props) => JSX.Element = (props: Props) => {
     }
     setIsShowCompleteDialog(true);
     resetState();
-  }
+  };
 
-  return(
+  return (
     <div
       className="fixed top-0 left-0 w-full h-full dialog-background"
-      id="exampleModal">
-        <div className="dialog p-6 rounded-xl m-16 mx-auto w-2/4">
-            <h1 className="text-xl font-bold mb-4">グループ作成</h1>
-            <div className="mb-4">
-              <TextInput label="グループ名*" placeHolder="Group Name" handleChangeValue={handleChangeGroupName} />
-            </div>
-            <div className="mb-4">
-              <TextInput label="パスワード*" placeHolder="Group Password" handleChangeValue={handleChangeGroupPassword} />
-            </div>
-            <div className="mb-4">
-              <TextInput label="サムネイル URL" placeHolder="Group Image URL" handleChangeValue={handleChangeGroupImageURL} />
-            </div>
-            <div className="mb-4">
-              <TextArea label="説明" placeHolder="Group Description" handleChangeValue={handleChangeGroupDetail} />
-            </div>
-            <div className="flex justify-around">
-              <button onClick={handleCloseDialog} className="px-4 py-2 rounded-full border hover:bg-gray-100 dark:hover:bg-gray-700">閉じる</button>
-              <button onClick={handleSubmit} className="bg-blue-400 text-white px-6 py-2 rounded-full hover:bg-blue-300">
-                作成
-              </button>
-            </div>
+      id="exampleModal"
+    >
+      <div className="dialog p-6 rounded-xl m-16 mx-auto w-2/4">
+        <h1 className="text-xl font-bold mb-4">グループ作成</h1>
+        <div className="mb-4">
+          <TextInput
+            label="グループ名*"
+            placeHolder="Group Name"
+            handleChangeValue={handleChangeGroupName}
+          />
         </div>
+        <div className="mb-4">
+          <TextInput
+            label="パスワード*"
+            placeHolder="Group Password"
+            handleChangeValue={handleChangeGroupPassword}
+          />
+        </div>
+        <div className="mb-4">
+          <TextInput
+            label="サムネイル URL"
+            placeHolder="Group Image URL"
+            handleChangeValue={handleChangeGroupImageURL}
+          />
+        </div>
+        <div className="mb-4">
+          <TextArea
+            label="説明"
+            placeHolder="Group Description"
+            handleChangeValue={handleChangeGroupDetail}
+          />
+        </div>
+        <div className="flex justify-around">
+          <button
+            onClick={handleCloseDialog}
+            className="px-4 py-2 rounded-full border hover:bg-gray-100 dark:hover:bg-gray-700"
+          >
+            閉じる
+          </button>
+          <button
+            onClick={handleSubmit}
+            className="bg-blue-400 text-white px-6 py-2 rounded-full hover:bg-blue-300"
+          >
+            作成
+          </button>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
-export default GroupAddDialog
+export default GroupAddDialog;
