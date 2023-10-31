a68edbd909ca42b60b2409a48f11f225a801c4bf
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-10-31 13:52:36 +0900

    iconの追加

---
+++ client/src/app/components/icons/back.tsx
@@ -0,0 +1,7 @@
+import { SVGProps } from "react";
+
+export function HeroiconsOutlineArrowLeft(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m10 19l-7-7m0 0l7-7m-7 7h18"></path></svg>
+  )
+}
